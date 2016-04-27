64f71b35ef0d73255d2b205f394210f058557efa
Alketa <alketta.c@live.com>
2016-04-27 20:58:02 +0200

    init

---
+++ TSE/src/main/java/com/example/TseApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		SpringApplication.run(TseApplication.class, args);
+	}
+}
