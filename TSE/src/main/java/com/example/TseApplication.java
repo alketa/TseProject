3e6737ed81865716868a5eab29c1ba26423f7133
Alketa <alketta.c@live.com>
2016-04-27 21:09:49 +0200

    Add an output change

--- TSE/src/main/java/com/example/TseApplication.java
+++ TSE/src/main/java/com/example/TseApplication.java
@@ -8,6 +8,7 @@ public class TseApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("Test second changes");
 		SpringApplication.run(TseApplication.class, args);
 	}
 }
